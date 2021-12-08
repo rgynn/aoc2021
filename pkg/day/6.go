76e10db9d2c5445242f2035fe039ad5b9cb4ca66
Rikard Gynnerstedt <rikard.gynnerstedt@gmail.com>
2021-12-08 09:04:11 +0100

    updated solution

--- pkg/day/6.go
+++ pkg/day/6.go
@@ -13,33 +13,39 @@ func SixPartTwo(input []string) (interface{}, error) {
 	return calc_fishes(256, input)
 }
 
-func calc_fishes(days int, input []string) (int, error) {
-	fishes, err := parse_fishes(input)
+func calc_fishes(num_days int, input []string) (int, error) {
+	days, err := parse_fish_days(input)
 	if err != nil {
 		return 0, err
 	}
-	for days > 0 {
-		for x := range fishes {
-			switch fishes[x] {
-			case 0:
-				fishes[x] = 6
-				fishes = append(fishes, 8)
-			default:
-				fishes[x]--
-			}
-		}
-		days--
+	for x := 0; x < num_days; x++ {
+		days = tick_fish_day(days)
 	}
-	return len(fishes), nil
+	var sum int
+	for _, day := range days {
+		sum += day
+	}
+	return sum, nil
 }
 
-func parse_fishes(input []string) (result []uint8, err error) {
-	for _, f := range strings.Split(input[0], ",") {
-		offset, err := strconv.Atoi(f)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, uint8(offset))
+func tick_fish_day(days []int) []int {
+	new_days := make([]int, 9)
+	for x := 1; x < 9; x++ {
+		new_days[x-1] = days[x]
 	}
-	return
+	new_days[6] += days[0]
+	new_days[8] += days[0]
+	return new_days
+}
+
+func parse_fish_days(input []string) ([]int, error) {
+	days := make([]int, 9)
+	for _, f := range strings.Split(input[0], ",") {
+		num, err := strconv.Atoi(f)
+		if err != nil {
+			return days, err
+		}
+		days[num]++
+	}
+	return days, nil
 }
